2497afd96da747ea4fd4895f06e52b125f60848d
Uğur CAN <ugurcanmail@yandex.com>
2023-04-06 20:09:44 +0300

    Update SundayAlgoritmasi.py

--- SundayAlgoritmasi.py
+++ SundayAlgoritmasi.py
@@ -1,22 +1,17 @@
-def check(char, pat, lenpat, textindex):
-    j = lenpat - 1
-    while j >= 0:
-        if pat[j] == char:
-            return textindex - j
-        j -= 1
-    return -1
-     
 def sundayAlg(text, pat):
-    t, p = len(text), len(pat)
+    t = len(text)
+    p = len(pat)
     i = 0
-    while i < t - p:
-        z = p - 1
-        while z >= 0 and pat[z] == text[i+z]:
-            z -= 1
-        if z == -1:
-            return i
-        i += check(text[i+p], pat, p, i+p)
-    return -1
-
+    while i <= t - p:
+        j = 0
+        while j < p and text[i+j] == pat[j]:
+            j += 1
+        if j == p:
+            last_index = i
+        if i + p < t:
+            i += p - pat.rfind(text[i + p])
+        else:
+            break
+    return last_index if 'last_index' in locals() else -1
 print(sundayAlg('mY5#E3g!F*6tI9Fg6tW*6t', 'W*6t'))
 print(sundayAlg('abcadabadccdbabd', 'cadab'))
